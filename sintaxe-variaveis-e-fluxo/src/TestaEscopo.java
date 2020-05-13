9d2d9e35613ba67ca934e0eac60770ab9e0a8c42
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2020-05-12 23:28:26 -0300

    Sintaxe - trabalhando com variaveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,15 @@
+
+public class TestaEscopo {
+	public static void main(String[] args) {
+		System.out.println("Testando Condicionais");
+		int idade = 20;
+		// int quantidadePessoas = 3;
+		boolean acompanhado = true; 
+		if(idade >=18 && acompanhado ) {
+			System.out.println("Você tem mais de 18 anos, bem vindo");
+		}
+		else {
+			System.out.println("Você não tem mais de 18 anos");
+		}
+	}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+	}
+
+}
