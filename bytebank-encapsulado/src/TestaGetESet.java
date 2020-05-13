395384e31c52f2c6e019cf0b3a8ac8e66f6786c8
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2020-05-12 23:06:16 -0300

    ByteBank - encapsulado - estudo de heranças e polimorfismo

---
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -0,0 +1,19 @@
+
+public class TestaGetESet {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 2226);
+		//conta.numero = 1337;
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo;
+		paulo.setNome("Paulo Silveira");
+		conta.setTitular(paulo);
+		System.out.println(conta.getTitular().getNome()); 
+		conta.getTitular().setProfissao("Profissão");
+		
+	}
+
+}
